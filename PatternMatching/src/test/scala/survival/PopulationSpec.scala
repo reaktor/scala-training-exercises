5661a71f11137dfd82d0245ee614bb753bbe3854
Lasse Koskela <lasse.koskela@reaktor.fi>
2013-04-05 15:18:24 +0300

    Remove the unused 'afters' from PopulationSpec.scala

--- PatternMatching/src/test/scala/survival/PopulationSpec.scala
+++ PatternMatching/src/test/scala/survival/PopulationSpec.scala
@@ -16,35 +16,30 @@ class PopulationSpec extends FlatSpec {
 
   "A lone lamb" should "grow and get older" in {
     val before = List(Lamb("Liisa", 1, 60))
-    val after = ScalaPopulation.next(before)
     val expected = List(Lamb("Liisa", 2, 61))
     assertPopulationEvolvesAsExpected(before, expected)
   }
 
   "Two wolves next to each other" should "fight and bigger wolf one should win" in {
     val before = List(Wolf("Pekka", 1, 80), Wolf("Jussi", 1, 85))
-    val after = ScalaPopulation.next(before)
     val expected = List(Wolf("Jussi", 2, 85))
     assertPopulationEvolvesAsExpected(before, expected)
   }
 
   "Wolf next to a lamb" should "eat the lamb and gain its weight" in {
     val before = List(Wolf("Pekka", 1, 80), Lamb("Liisa", 1, 60))
-    val after = ScalaPopulation.next(before)
     val expected = List(Wolf("Pekka", 2, 140))
     assertPopulationEvolvesAsExpected(before, expected)
   }
 
   "Lamb next to a wolf" should "get eaten and the wolf should gain its weight" in {
     val before = List(Lamb("Liisa", 1, 60), Wolf("Pekka", 1, 80))
-    val after = ScalaPopulation.next(before)
     val expected = List(Wolf("Pekka", 2, 140))
     assertPopulationEvolvesAsExpected(before, expected)
   }
 
   "Two lambs" should "live together happily and grow bigger and older" in {
     val before = List(Lamb("Liisa", 1, 60), Lamb("Maija", 1, 55))
-    val after = ScalaPopulation.next(before)
     val expected = List(Lamb("Liisa", 2, 61), Lamb("Maija", 2, 56))
     assertPopulationEvolvesAsExpected(before, expected)
   }
